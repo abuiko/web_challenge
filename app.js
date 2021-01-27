844c990021d764e28beabca4dfc73940ef57edff
Alena Buiko <abuiko.ma@gmail.com>
2021-01-27 16:18:57 -0500

    open menu on burger click in mobile

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-
+    const burger = document.querySelector(".header__toggle");
+    const hiddenMenu = document.querySelector(".hidden-menu");
+    const overlay = document.querySelector(".overlay");
+    burger.addEventListener('click', () => {
+        hiddenMenu.classList.toggle('open');
+        if (hiddenMenu.classList.contains('open')) {
+            overlay.style.opacity = 1;
+            burger.classList.add('active');
+        } else {
+            overlay.style.opacity = 0;
+            burger.classList.remove('active');
+        }
+    })
 
 
 
